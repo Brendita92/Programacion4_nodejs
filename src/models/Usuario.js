6f73d91c904d0134a339e0825c3c803838b52924
Geraltv8 <geraquesearock@gmail.com>
2026-09-17 14:49:09 -0300

    agregado middleware para hasheo y comparacion de contraseña

--- src/models/Usuario.js
+++ src/models/Usuario.js
@@ -21,4 +21,23 @@ const usuarioSchema = new mongoose.Schema({
     timestamps: true
 });
 
+usuarioSchema.pre('save', async function(next) {
+
+    if (!this.isModified('password')) return next()
+
+    try {
+        const salt = await bcrypt.genSalt(10);
+
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
+
+usuarioSchema.methods.compararPassword = async function(passwordIgresado) {
+    return await bcrypt.compare(passwordIgresado, this.password);
+};
+
+
 export const Usuario = mongoose.model('Usuario', usuarioSchema);
